27160b7cc3f5ad5b8e930c15f760e296f5f5b811
Luciano Cordeiro <lacs@petrobras.com.br>
2022-06-19 10:37:34 -0300

    Adding the revalidate property

--- pages/tempo.js
+++ pages/tempo.js
@@ -9,14 +9,18 @@ function Tempo(props) {
 
   return (
     <div id="tempo">
-      <div id="tempo-din"> {dynamicDateString} (dinâmico) 
-      </div>
+      <div id="tempo-din"> {dynamicDateString} (dinâmico)   </div>
       <div id="tempo-est"> {props.staticDateString} (estático) </div>
     </div>
   )
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+
+  console.log('> Passando pelo getStaticProps();');
+ // console.log('> Adicionando um delay de 5 segundos');
+
+  //await delay(5000);
 
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
@@ -24,8 +28,10 @@ export function getStaticProps() {
   return {
     props: {
       staticDateString
-    }
+    }, revalidate: 1
   }
 }
+
+//const delay = ms => new Promise(resolve => setTimeout(resolve,ms));
 
 export default Tempo;
